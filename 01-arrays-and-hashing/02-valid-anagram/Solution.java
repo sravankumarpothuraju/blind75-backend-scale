c022731487b6f8e0be89314573d03e8a05e28b6a
sravankumarpothuraju <sravan.pothuraju@gmail.com>
2026-07-12 19:37:02 -0400

    feat(valid-anagram): implement brute force O(N^2) solution

--- 01-arrays-and-hashing/02-valid-anagram/Solution.java
+++ 01-arrays-and-hashing/02-valid-anagram/Solution.java
@@ -2,7 +2,48 @@ package arraysandhashing.validanagram;
 
 public class Solution {
     public boolean isAnagram(String s, String t) {
-        // TODO: Implement solution
-        return false;
+        if (s.length() != t.length()) {
+
+            return false;
+        }
+
+        if (s.isBlank() || t.isBlank()) {
+            return false;
+        }
+
+
+        for (int i = 0; i < s.length(); i++) {
+            int countOfS = 0;
+            int countOfT = 0;
+
+            char targetChar = s.charAt(i);
+
+
+            for (int j = 0; j < s.length(); j++) {
+
+                if (targetChar == s.charAt(j)) {
+
+                    countOfS++;
+                }
+            }
+
+
+            for (int k = 0; k < t.length(); k++) {
+
+                if (targetChar == t.charAt(k)) {
+
+                    countOfT++;
+                }
+            }
+
+
+            if (countOfT != countOfS) {
+
+                return false;
+            }
+
+        }
+
+        return true;
     }
 }
