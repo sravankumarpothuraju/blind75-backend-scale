14482b59756cccb5348dd525647ed2066ad147ce
sravankumarpothuraju <sravan.pothuraju@gmail.com>
2026-08-10 23:46:54 -0400

    feat(two-sum): implement brute force O(N^2) solution

--- 01-arrays-and-hashing/03-two-sum/Solution.java
+++ 01-arrays-and-hashing/03-two-sum/Solution.java
@@ -2,7 +2,16 @@ package arraysandhashing.twosum;
 
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // TODO: Implement solution
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] + nums[j] == target) {
+
+                    return new int[]{i, j};
+                }
+            }
+        }
         return new int[0];
     }
 }
